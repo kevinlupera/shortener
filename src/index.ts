bb67b5721994495136ca61b090e3f5bb9ea6eaea
Kevin Lupera <kevinlupera@gmail.com>
2024-08-28 23:20:38 -0500

    chore: Add Hono and itty-router dependencies, update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Context, Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono();
+
+app.get('/:slug', async (c: Context) => {
+	const slug = c.req.param('slug');
+	const redirectTo = await c.env.SHORTENER_KV.get(slug);
+
+	if (redirectTo) {
+		return c.redirect(redirectTo, 302);
+	}
+	return c.json('URL not found', {
+		status: 404,
+	});
+});
+
+app.post('/', async (c: Context) => {
+	const data = await c.req.json();
+	const longLink = data?.url;
+	let slug = Math.random().toString(36).substring(7);
+	let existing = await c.env.SHORTENER_KV.get(slug);
+	while (existing) {
+		slug = Math.random().toString(36).substring(7);
+		existing = await c.env.SHORTENER_KV.get(slug);
+	}
+
+	await c.env.SHORTENER_KV.put(slug, longLink);
+
+	return c.json({ url: `${c.env.HOST_URL}/${slug}` });
+});
+
+export default app;
